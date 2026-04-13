6044424d6808907f95b17982b916eb12cb8a62ec
Fernando <fernando.e.mejia.e@gmail.com>
2026-04-13 13:17:54 -0600

    User interface feedback alerts

--- src/features/home/page/Prove.jsx
+++ src/features/home/page/Prove.jsx
@@ -1,45 +1,20 @@
-// CodeEditor 
+// Prove 
 import React, { useState } from "react";
+import AlertFeedback from "@/ui/AlertFeedback";
 
-const defaultCode = `
-<div class="p-4 bg-blue-500 text-white rounded-lg">
-  Editor de código con Tailwind!
-</div>
-`;
+export default function Prove() {
 
-export default function CodeEditor() {
-  const [code, setCode] = useState(defaultCode);
-
+  const [show, setShow] = useState(true);
   return (
     <div className="grid grid-cols-2 gap-4 p-4 bg-gray-100 h-dvh">
- 
-      <div className="flex flex-col">
-        <h2 className="mb-2 text-2xl font-bold">Editor Tailwind</h2>
-        <textarea
-          value={code}
-          onChange={(e) => setCode(e.target.value)}
-          className="flex-1 p-4 font-mono bg-white border border-gray-300 rounded-md shadow resize-none"
-          style={{ minHeight: "400px" }}
-        ></textarea>
-      </div>
-  
-      <div className="flex flex-col">
-        <h2 className="mb-2 text-2xl font-bold">Vista previa</h2>
-        <iframe
-          className="flex-1 bg-white border border-gray-300 rounded-md shadow"
-          style={{ minHeight: "400px" }}
-          srcDoc={`
-            <html>
-              <head>
-                <script src="https://cdn.tailwindcss.com"></script>
-              </head>
-              <body class="p-4">
-                ${code}
-              </body>
-            </html>
-          `}
-        ></iframe>
-      </div>
+
+      {show && (
+        <AlertFeedback
+          message="lorem ipsum dolor sit amet!"
+          onClose={() => setShow(false)}
+        />
+      )}
+
     </div>
   );
 }
---
+++ src/ui/AlertFeedback.jsx
@@ -0,0 +1,76 @@
+import { useEffect, useState } from "react";
+
+const styles = {
+  success: {
+    border: "border-cyan-500",
+    text: "text-cyan-600",
+    face: "Face1.webp",
+  },
+  warning: {
+    border: "border-yellow-500",
+    text: "text-yellow-600",
+    face: "Face3.webp",
+  },
+  error: {
+    border: "border-red-500",
+    text: "text-red-600",
+    face: "Face2.webp",
+  }
+};
+
+const AlertFeedback = ({ message, type = "error", duration = 5000, onClose }) => {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    setVisible(true);
+
+    const timer = setTimeout(() => {
+      setVisible(false);
+
+      setTimeout(() => {
+        onClose && onClose();
+      }, 300);
+    }, duration);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div
+      className={`
+        fixed top-5 right-5 z-50
+        transition-all duration-300 ease-in-out
+        ${visible ? "translate-x-0 opacity-100" : "translate-x-full opacity-0"}
+      `}
+    >
+      <div className={`flex items-center gap-2 p-4 text-white border-l-4 rounded-lg shadow-lg bg-neutral-900 ${styles[type]?.border || styles.success.border}`}>
+        
+        <svg
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          fill="none"
+          className={`flex-shrink-0 w-5 h-5 ${styles[type]?.text || styles.success.text}`}
+        >
+          <path
+            d="M13 16h-1v-4h1m0-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            strokeWidth="2"
+            strokeLinejoin="round"
+            strokeLinecap="round"
+          />
+        </svg>
+
+        <p className="text-base font-semibold max-w-96">
+          {message}
+        </p>
+
+        <img
+          src={`/img/${styles[type]?.face || styles.success.face}`}
+          alt="Face"
+          className="object-contain w-12 h-12"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AlertFeedback;
