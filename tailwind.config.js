6f8feefbc4960402a21ca4f16ae286e6ccfbd191
Fernando <fernandomejiadev@gmail.com>
2026-05-18 08:40:50 -0600

    Tailwind Config Setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,25 +4,7 @@ module.exports = {
   darkMode: "class",
   theme: {
     extend: {
-      colors: {
-        beige: {
-          50: "#f3efe6",
-          100: "#ede7db",
-          200: "#e2d9c8",
-          300: "#d6cbb9",
-          400: "#cfc4b0",
-          500: "#c1b39e",
-          600: "#b9ad93",
-          700: "#a49580",
-          800: "#9c8e74",
-          900: "#8b7b67",
-          950: "#4b4236",
-        },
-      },
-      fontFamily: {
-        fantasy: ["Fantasy", "cursive"],
-        antonio: ["Antonio"], 
-      },
+    
     },
   },
   plugins: [],
