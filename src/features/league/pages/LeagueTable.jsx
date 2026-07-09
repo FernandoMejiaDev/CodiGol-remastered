5cc0ffda34d74cff5066c7d41f0224f9cc90dd07
Fernando <fernandomejiadev@gmail.com>
2026-07-09 10:12:56 -0600

    Improvements to the league table, adding more data and improving its design

--- src/features/league/pages/LeagueTable.jsx
+++ src/features/league/pages/LeagueTable.jsx
@@ -62,6 +62,9 @@ const LeagueTable = () => {
         draws: 0,
         losses: 0,
         points: 0,
+        goalsFor: "-",
+        goalsAgainst: "-",
+        goalDifference: "-",
       };
     });
 
@@ -79,8 +82,8 @@ const LeagueTable = () => {
         home.losses++;
       } else if (result === "draw") {
         home.draws++;
-        home.points += 1;
         away.draws++;
+        home.points += 1;
         away.points += 1;
       }
     });
@@ -92,64 +95,155 @@ const LeagueTable = () => {
     (a, b) => b.points - a.points
   );
 
+  /*
   const navigate = useNavigate();
-  
-    useEffect(() => {
-      unlockNextPage("/FinalScene");
-  
-      const timeout = setTimeout(() => {
-        navigateToNextPhase("LeagueTable", navigate);
-      }, 20000);
-  
-      return () => clearTimeout(timeout);
-    }, [navigate]);
+
+  useEffect(() => {
+    //unlockNextPage("/FinalScene");
+
+    const timeout = setTimeout(() => {
+      navigateToNextPhase("LeagueTable", navigate);
+    }, 20000);
+    
+    return () => clearTimeout(timeout);
+  }, [navigate]);
+    */
 
   return (
-    <div className="relative w-full h-dvh overflow-hidden">
+    <div className="relative flex flex-col p-6 m-auto text-white h-dvh">
+
       <div
         className="absolute inset-0 bg-fixed bg-center bg-cover"
         style={{ backgroundImage: `url(/img/Background.webp)` }}
       ></div>
 
-      <div className="flex flex-row h-full">
+      <div className="relative flex flex-col justify-center h-full m-auto max-w-7xl">
 
-        {/* <ModalSize className="absolute" /> */}
+        <h1 className="mb-6 text-4xl font-bold text-center text-white drop-shadow-lg">
+          Tabla de Resultados
+        </h1>
 
-        <div className="relative text-white flex flex-col text-center p-6 m-auto max-w-[60rem]">
-          <h1 className="mb-6 text-4xl font-bold text-white drop-shadow-lg">
-            Tabla de Resultados
-          </h1>
+        <div
+          className="
+      grid
+      grid-cols-[60px_2.5fr_repeat(8,70px)]
+      items-center
+      gap-2
+      p-3
+      font-bold
+      bg-neutral-950/70
+      rounded-t-2xl
+    "
+        >
+          <div className="text-center">Pos</div>
+          <div>Equipo</div>
 
-          <div className="grid grid-cols-6 gap-4 p-3 font-bold bg-neutral-950/60 rounded-t-2xl">
-            <div className="col-span-2 text-center">Equipo</div>
-            <div className="text-center">Ganados</div>
-            <div className="text-center">Empates</div>
-            <div className="text-center">Perdidos</div>
-            <div className="text-center">Puntos</div>
+          <div className="text-center">PJ</div>
+          <div className="text-center">G</div>
+          <div className="text-center">E</div>
+          <div className="text-center">P</div>
+
+          <div className="text-center">GF</div>
+          <div className="text-center">GC</div>
+          <div className="text-center">DG</div>
+
+          <div className="text-center">Pts</div>
+        </div>
+
+        {leagueTable.map((team, index) => (
+          <div
+            key={team.id}
+            className={`
+        grid
+        grid-cols-[60px_2.5fr_repeat(8,70px)]
+        items-center
+        gap-2
+        p-3
+        transition
+        bg-neutral-950/60
+
+                ${
+    index === 0
+      ? "bg-cyan-900/80 border-l-4 border-cyan-600"
+      : index === leagueTable.length - 1
+      ? "bg-red-900/80 border-l-4 border-red-600"
+      : ""
+  }
+
+        hover:bg-neutral-950/80
+
+        ${index === leagueTable.length - 1
+                ? "rounded-b-2xl"
+                : ""
+              }
+      `}
+          >
+
+            {/* Table position */}
+            <div
+              className={"text-center font-bold"}
+            >
+              {index + 1}
+            </div>
+
+            <div className="flex items-center">
+              <img
+                src={team.logo}
+                alt={team.name}
+                className="object-contain w-10 h-10 mr-3"
+                loading="lazy"
+              />
+
+              <span className="font-medium">
+                {team.name}
+              </span>
+            </div>
+
+            {/* PJ */}
+            <div className="text-center">
+              {team.wins + team.draws + team.losses}
+            </div>
+
+            {/* G */}
+            <div className="text-center">
+              {team.wins}
+            </div>
+
+            {/* E */}
+            <div className="text-center">
+              {team.draws}
+            </div>
+
+            {/* P */}
+            <div className="text-center">
+              {team.losses}
+            </div>
+
+            {/* GF */}
+            <div className="text-center">
+              {team.goalsFor}
+            </div>
+
+            {/* GC */}
+            <div className="text-center">
+              {team.goalsAgainst}
+            </div>
+
+            {/* DG */}
+            <div className="text-center">
+              {team.goalDifference}
+            </div>
+
+            {/* points */}
+            <div className="text-xl font-bold text-center ">
+              {team.points}
+            </div>
+
           </div>
 
-          {leagueTable.map((team) => (
-            <div
-              key={team.id}
-              className="grid items-center grid-cols-6 gap-4 p-3 transition last-of-type:rounded-b-2xl bg-neutral-950/60 hover:bg-neutral-950/70"
-            >
-              <div className="flex items-center col-span-2">
-                <img
-                  src={team.logo}
-                  alt={team.name}
-                  className="object-contain w-10 h-10 mr-2"
-                  loading="lazy"
-                />
-                <span className="font-medium">{team.name}</span>
-              </div>
-              <div className="text-center">{team.wins}</div>
-              <div className="text-center">{team.draws}</div>
-              <div className="text-center">{team.losses}</div>
-              <div className="font-bold text-center">{team.points}</div>
-            </div>
-          ))}
-        </div>
+        ))}
       </div>
+
     </div>
   );
 };
