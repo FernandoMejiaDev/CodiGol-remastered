bf36b51167dcbbe76f378c9e67a468b55b673015
Fernando <fernandomejiadev@gmail.com>
2026-07-29 12:36:44 -0600

    Improvements in match days, greater clarity

--- src/features/league/data/fixtures.js
+++ src/features/league/data/fixtures.js
@@ -4,6 +4,7 @@ import { TEAM } from "@/features/matches/data/teams/teamIds";
 
 export const fixtures = {
   1: [
+    { homeId: TEAM.WIND_JAGUARS, awayId:TEAM.GEM_RUBIES }, // Wind Jaguars vs Gem Rubies
     { homeId: TEAM.TUSK_ELEPHANTS, awayId: TEAM.JUGGERNAUT_LEOPARD },
     { homeId: TEAM.CODE_SHARKS, awayId: TEAM.SOAR_SWIFTS },
     { homeId: TEAM.KNIGHT_CODERS, awayId: TEAM.BYTE_GOPHERS },
@@ -12,19 +13,20 @@ export const fixtures = {
   ],
 
   2: [
+    { homeId: TEAM.GEM_RUBIES, awayId: TEAM.WEB_HAWKS }, 
+    { homeId: TEAM.WIND_JAGUARS, awayId: TEAM.WEB_HAWKS }, // Wind Jaguars vs Web Hawks
     { homeId: TEAM.TUSK_ELEPHANTS, awayId: TEAM.CODE_SHARKS },
     { homeId: TEAM.JUGGERNAUT_LEOPARD, awayId: TEAM.KNIGHT_CODERS },
     { homeId: TEAM.SOAR_SWIFTS, awayId: TEAM.STYLE_CHAMELEONS },
     { homeId: TEAM.BYTE_GOPHERS, awayId: TEAM.FORGE_MINOTAURS },
-    { homeId: TEAM.PHANTOM_VIPER, awayId: TEAM.WEB_HAWKS },
   ],
 
   3: [
     { homeId: TEAM.GEM_RUBIES, awayId: TEAM.WEB_HAWKS },
+    { homeId: TEAM.WIND_JAGUARS, awayId: TEAM.CODE_SHARKS }, // Wind Jaguars vs Code Sharks
     { homeId: TEAM.TUSK_ELEPHANTS, awayId: TEAM.FORGE_MINOTAURS },
     { homeId: TEAM.JUGGERNAUT_LEOPARD, awayId: TEAM.PHANTOM_VIPER },
-    { homeId: TEAM.CODE_SHARKS, awayId: TEAM.STYLE_CHAMELEONS },
-    { homeId: TEAM.SOAR_SWIFTS, awayId: TEAM.BYTE_GOPHERS },
+    { homeId: TEAM.SOAR_SWIFTS, awayId: TEAM.STYLE_CHAMELEONS },
+    { homeId: TEAM.BYTE_GOPHERS, awayId: TEAM.KNIGHT_CODERS },
   ],
-
 };
