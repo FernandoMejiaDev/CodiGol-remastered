9d0db7fb08260e6efe3dcf09706d0e8eca83e242
Fernando <fernandomejiadev@gmail.com>
2026-07-29 11:28:05 -0600

    File to collect all equipment and match day files.

---
+++ src/features/league/data/fixtures.js
@@ -0,0 +1,13 @@
+// Match schedule for rounds 1 to 3
+
+export const fixtures = {
+  1: [
+    { homeId: 3, awayId: 4 },
+    { homeId: 5, awayId: 6 },
+    { homeId: 7, awayId: 8 },
+    { homeId: 9, awayId: 10 },
+    { homeId: 11, awayId: 12 },
+  ],
+
+
+};
---
+++ src/features/matches/data/teams/index.js
@@ -0,0 +1,29 @@
+//This file includes all equipment
+
+import WindJaguars from "@/features/matches/data/Wind_Jaguars";
+import GemRubies from "@/features/matches/data/Gem_Rubies";
+import TuskElephants from "@/features/matches/data/Tusk_Elephants";
+import JuggernautLeopard from "@/features/matches/data/Juggernaut_Leopard";
+import CodeSharks from "@/features/matches/data/Code_Sharks";
+import SoarSwifts from "@/features/matches/data/Soar_Swifts";
+import KnightCoders from "@/features/matches/data/Knight_Coders";
+import ByteGophers from "@/features/matches/data/Byte_Gophers";
+import StyleChameleons from "@/features/matches/data/Style_Chameleons";
+import PhantomViper from "@/features/matches/data/Phantom_Viper";
+import ForgeMinotaurs from "@/features/matches/data/Forge_Minotaurs";
+import WebHawks from "@/features/matches/data/Web_Hawks";
+
+export const teams = [
+  WindJaguars,
+  GemRubies,
+  TuskElephants,
+  JuggernautLeopard,
+  CodeSharks,
+  SoarSwifts,
+  KnightCoders,
+  ByteGophers,
+  StyleChameleons,
+  PhantomViper,
+  ForgeMinotaurs,
+  WebHawks,
+];
