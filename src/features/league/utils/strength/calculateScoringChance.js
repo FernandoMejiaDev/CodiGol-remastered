c639a9d12fd18ec15722ca646a27530ae9b3f9c5
Fernando <fernandomejiadev@gmail.com>
2026-08-16 11:43:55 -0600

    Calculate Scoring Chance Function

--- src/features/league/utils/strength/calculateScoringChance.js
+++ src/features/league/utils/strength/calculateScoringChance.js
@@ -19,3 +19,24 @@ export const calculateScoringChance = (localTeam, visitorTeam) => {
   };
 };
 
+/*
+calculate Possession Chance file
+
+To calculate possession, each team has 3 properties of type number (strength, attack and defense)
+To calculate, we divide the local attack by the sum of the local attack and the visiting defense.
+localTeam.attack / (localTeam.attack + visitorTeam.defense)
+And the opposite for the visiting team.
+visitorTeam.attack / (visitorTeam.attack + localTeam.defense)
+The result will give us the percentage of possession for each team
+
+calculate Scoring Chance File
+
+The probability of scoring refers to the likelihood that the team will create scoring opportunities.
+
+The formula is (local attack * local strength) divided by (local attack * local strength) and the sum of (visiting defense * visiting strength)
+
+Formula:
+localTeam.attack * localTeam.Strength / (localTeam.attack * localTeam.Strength) + (localTeam.defense * visitorTeam.strength)
+
+Reverse formula for the visiting team
+*/
