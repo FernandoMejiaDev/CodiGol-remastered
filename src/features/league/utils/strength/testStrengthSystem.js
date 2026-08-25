e9d0ed1e9c926340671c88c0ca46e3d01da3a569
Fernando <fernandomejiadev@gmail.com>
2026-08-25 09:30:40 -0600

    bug printing 2 times in console match simulation

--- src/features/league/utils/strength/testStrengthSystem.js
+++ src/features/league/utils/strength/testStrengthSystem.js
@@ -1,3 +1,4 @@
+import { useEffect, useRef} from "react";
 import TuskElephants from "@/features/matches/data/teams/tusk_elephants";
 import PhantomViper from "@/features/matches/data/teams/phantom_viper";
 import CodeSharks from "@/features/matches/data/teams/code_sharks";
@@ -7,77 +8,99 @@ import { calculateMatchResult } from "@/features/league/utils/strength/calculate
 
 const TestStrengthSystem = () => {
 
-  const simulateMatches = (
-    localTeam,
-    visitorTeam,
-    numberOfMatches = 1000
-  ) => {
-    let localWins = 0;
-    let draws = 0;
-    let visitorWins = 0;
+  const hasRun = useRef(false);
 
-    let totalLocalGoals = 0;
-    let totalVisitorGoals = 0;
+  useEffect(() => {
 
-    for (let i = 0; i < numberOfMatches; i++) {
+     if (hasRun.current) return;
 
-      const result = calculateMatchResult(
-        localTeam,
-        visitorTeam
+    hasRun.current = true;
+
+    const simulateMatches = (
+      localTeam,
+      visitorTeam,
+      //100 matches to analyze the result and see the correct 
+      // functioning of the equations
+      numberOfMatches = 100 
+    ) => {
+
+      let localWins = 0;
+      let draws = 0;
+      let visitorWins = 0;
+
+      let totalLocalGoals = 0;
+      let totalVisitorGoals = 0;
+
+      for (let i = 0; i < numberOfMatches; i++) {
+
+        const result = calculateMatchResult(
+          localTeam,
+          visitorTeam
+        );
+
+        totalLocalGoals += result.localGoals;
+        totalVisitorGoals += result.visitorGoals;
+
+        if (result.localGoals > result.visitorGoals) {
+          localWins++;
+        } else if (result.localGoals < result.visitorGoals) {
+          visitorWins++;
+        } else {
+          draws++;
+        }
+      }
+
+      console.log("===");
+
+      console.log(
+        `${localTeam.name} VS ${visitorTeam.name}`
       );
 
-      totalLocalGoals += result.localGoals;
-      totalVisitorGoals += result.visitorGoals;
+      console.log("Partidos:", numberOfMatches);
+      console.log("Victorias local:", localWins);
+      console.log("Empates:", draws);
+      console.log("Victorias visitante:", visitorWins);
 
-      if (result.localGoals > result.visitorGoals) {
-        localWins++;
-      } else if (result.localGoals < result.visitorGoals) {
-        visitorWins++;
-      } else {
-        draws++;
-      }
-    }
+      console.log(
+        "Promedio goles local:",
+        totalLocalGoals / numberOfMatches
+      );
 
-    console.log("===");
-    console.log(
-      `${localTeam.name} VS ${visitorTeam.name}`
+      console.log(
+        "Promedio goles visitante:",
+        totalVisitorGoals / numberOfMatches
+      );
+    };
+
+    const result = calculateMatchResult(
+      PhantomViper,
+      TuskElephants
     );
 
-    console.log("Partidos:", numberOfMatches);
-    console.log("Victorias local:", localWins);
-    console.log("Empates:", draws);
-    console.log("Victorias visitante:", visitorWins);
+    console.log("PHANTOM VIPER VS TUSK ELEPHANTS");
+    console.log(result);
 
-    console.log(
-      "Promedio goles local:",
-      totalLocalGoals / numberOfMatches
+    simulateMatches(
+      PhantomViper,
+      TuskElephants,
+      100
     );
 
-    console.log(
-      "Promedio goles visitante:",
-      totalVisitorGoals / numberOfMatches
+    const result2 = calculateMatchResult(
+      CodeSharks,
+      KnightCoders
     );
-  };
 
-  const result = calculateMatchResult(
-    PhantomViper,
-    TuskElephants
-  );
+    console.log("CODE SHARKS VS KNIGHT CODERS");
+    console.log(result2);
 
-  console.log("PHANTOM VIPER VS TUSK ELEPHANTS");
-  console.log(result);
+    simulateMatches(
+      CodeSharks,
+      KnightCoders,
+      100
+    );
 
-  simulateMatches(
-    PhantomViper,
-    TuskElephants,
-    1000
-  );
-
-  simulateMatches(
-    CodeSharks,
-    KnightCoders,
-    1000
-  );
+  }, []);
 
   return null;
 };
